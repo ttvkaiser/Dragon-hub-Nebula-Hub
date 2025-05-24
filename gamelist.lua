b74ad05d94917ee1f467f98b372969b8d8a362dd
ttvkaiser <vaeros694@gmail.com>
2025-05-24 17:29:03 -0600

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -2,9 +2,9 @@ local Games = {
 
 [16732694052] = "https://raw.githubusercontent.com/ttvkaiser/Dragon-hub-Nebula-Hub/refs/heads/main/Fisch-main.lua", -- Fisch
 [536102540] = "", -- Muscle Legends
-[2753915549] = "", -- Blox Fruits (First sea)
-[4442272183] = "", -- Blox Fruits (Second sea)
-[7449423635] = "", -- Blox Fruits (Third sea)
+[2753915549] = "https://raw.githubusercontent.com/ttvkaiser/Dragon-hub-Nebula-Hub/refs/heads/main/Blox%20fruits%20first%20sea.lua", -- Blox Fruits (First sea)
+[4442272183] = "https://raw.githubusercontent.com/ttvkaiser/Dragon-hub-Nebula-Hub/refs/heads/main/Blox%20fruits%20second%20sea.lua", -- Blox Fruits (Second sea)
+[7449423635] = "https://raw.githubusercontent.com/ttvkaiser/Dragon-hub-Nebula-Hub/refs/heads/main/Blox%20fruits%20third%20sea.lua", -- Blox Fruits (Third sea)
 
 }
 
