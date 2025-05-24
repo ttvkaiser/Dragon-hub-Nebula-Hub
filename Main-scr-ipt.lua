05ce3dc6c48df33faacf048daf079b99f01a6a9c
ttvkaiser <vaeros694@gmail.com>
2025-05-24 13:01:03 -0600

    Update Main-scr-ipt.lua

--- Main-scr-ipt.lua
+++ Main-scr-ipt.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet(""))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ttvkaiser/Dragon-hub-Nebula-Hub/refs/heads/main/gamelist.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
