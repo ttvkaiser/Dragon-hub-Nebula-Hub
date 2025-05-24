54fffa949a1f2850aabaa3afae8005a7e593dfc4
ttvkaiser <vaeros694@gmail.com>
2025-05-24 13:02:43 -0600

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,6 +1,6 @@
 local Games = {
 
-[16732694052] = "https://pandadevelopment.net/virtual/file/42737a952c22cd39", -- Fisch
+[16732694052] = "https://raw.githubusercontent.com/ttvkaiser/Dragon-hub-Nebula-Hub/refs/heads/main/Fisch-main.lua", -- Fisch
 [536102540] = "", -- Muscle Legends
 
 }
