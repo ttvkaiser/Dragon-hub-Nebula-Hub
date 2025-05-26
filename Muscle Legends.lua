23b8ded4151306012e7aa5863e105814790e3c2f
ttvkaiser <vaeros694@gmail.com>
2025-05-25 23:17:12 -0600

    Update Muscle Legends.lua

--- Muscle Legends.lua
+++ Muscle Legends.lua
@@ -1,6 +1,6 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/memejames/elerium-v2-ui-library/main/Library", true))()
 
-local window = library:AddWindow("Dragon Hub | Game: Muscle Legends | Version [v.1.2.0] | by FLX_Liam", {
+local window = library:AddWindow("Dragon Hub | Game: Muscle Legends | Version [v.1.2.0] | by FLX_Liam & ttvkaiser", {
     main_color = Color3.fromRGB(50, 85, 135),
     min_size = Vector2.new(720, 560),
     can_resize = true
